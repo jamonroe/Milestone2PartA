41cfc3e5880443341b6f647cb0f45e5301674d2d
christyeicher <christyeicher@yahoo.com>
2014-03-28 01:25:27 -0700

    Modified print statement :B

--- src/SampleClass.java
+++ src/SampleClass.java
@@ -2,7 +2,7 @@
 public class SampleClass {
 
 	public static void main(String[] args) {
-		System.out.println("I've made changes to the repo!");
+		System.out.println("I've made changes to the repo! Yay!");
 	}
 	
 }
