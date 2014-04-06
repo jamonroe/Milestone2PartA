54055fae9e91a3c497bfc6cc2579445ffbd0fe40
jamonroe <jasonmonroe90@gmail.com>
2014-04-06 16:46:40 -0700

    Added gson to our project to parse the JSON file.  Added a printMap method to demonstrate how to access the keys and values.  Values are always in the form of String or Map<String>

--- src/Udacity.java
+++ src/Udacity.java
@@ -1,24 +1,21 @@
 import org.apache.commons.io.IOUtils;
-import org.jsoup.*;
-import org.jsoup.nodes.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import com.google.gson.Gson;
+
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Udacity {
-
+	
 	public static void main(String[] args) {
 		ArrayList<String> urls = fetchURLs();
-		for (String s : urls)
-			System.out.println(s);
+		//for (String s : urls)
+			//System.out.println(s);
 	}
 	
 	public static ArrayList<String> fetchURLs() {
@@ -28,47 +25,34 @@ public class Udacity {
 	    try {
 	        url = new URL("https://www.udacity.com/api/nodes?depth=2&fresh=false&keys[]=course_catalog&projection=catalog&required_behavior=find");
 
-			System.out.println("Attempting to fetch course links from source: " + url);
-
 			is = url.openStream();
 	        String index = IOUtils.toString(is);
+
+	        Pattern p = Pattern.compile("(?s)\\{.*");
+	        Matcher m = p.matcher(index);
 	        
-	        //System.out.println(index);
+	    	Gson gson = new Gson();
+	        printMap((Map)gson.fromJson(index, Object.class));
 	        
-	        String[] references = index.split("ref");
-	        
-	        Pattern keyPattern = Pattern.compile("(?i)\"key\":.\"([a-z]|[0-9])*\"");
-	        Matcher m;
-	        
-	        ArrayList<String> urls = new ArrayList<String>();
-	        
-	        for (String s : references) {
-	        	m = keyPattern.matcher(s);
-	        	if (m.find()) {
-	        		urls.add("https://www.udacity.com/course/" + m.group().replaceAll("(\"|\\s)", "").split(":")[1]);
-	        	} else {
-	        		//System.out.println("No URL found" + s);
-	        	}
-	        }
-	        sortUniques(urls);
-	        return urls;
-	    } catch (MalformedURLException mue) {
-	         mue.printStackTrace();
-	    } catch (IOException ioe) {
-	         ioe.printStackTrace();
-	    } finally {
-	         if (is != null) IOUtils.closeQuietly(is);
+	    } catch (Exception e) {
+	    	e.printStackTrace();
 	    }
 	    return null;
 	}
 	
-	public static void sortUniques(ArrayList<? extends String> list) {
-        Collections.sort(list);
-		for (int i = 0; i < list.size() - 1; i++) {
-			if (list.get(i).equals(list.get(i+1))) {
-				list.remove(i);
-				i--;
+	public static void printMap(Map m) {
+		if (m == null) return;
+		Set s = m.keySet();
+		Object val;
+		for (Object key : s) {
+			val = m.get(key);
+			if (val instanceof Map) {
+				System.out.println(key + ":\n{");
+				printMap((Map) val);
+				System.out.println("}");
 			}
+			else
+				System.out.println(key + ": " + val);
 		}
 	}
 }
